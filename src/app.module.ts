611ed872e55b5bf60c4b8c9dc73f5bc97c46b3d3
Matheus Mota <devmatheusmota@gmail.com>
2023-04-15 20:48:34 -0300

    feat: modules base folder has been created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { BodyMiddleware } from './middlewares/body.middleware';
+import { StudentModule } from './modules/student/student.module';
+import { AddressModule } from './modules/address/address.module';
+import { TeacherModule } from './modules/teacher/teacher.module';
+import { SubjectModule } from './modules/subject/subject.module';
+import { ClassModule } from './modules/class/class.module';
+import { ActivityModule } from './modules/activity/activity.module';
+import { GradeModule } from './modules/grade/grade.module';
 
 @Module({
-  imports: [],
+  imports: [
+    StudentModule,
+    AddressModule,
+    TeacherModule,
+    SubjectModule,
+    ClassModule,
+    ActivityModule,
+    GradeModule,
+  ],
   controllers: [],
   providers: [],
 })
